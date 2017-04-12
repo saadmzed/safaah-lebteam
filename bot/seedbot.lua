1f3924725d7de00d725cfde8bcde1fbd962f28bf
saadmzed <saad5mzed@gmail.com>
2017-04-12 22:39:36 +0300

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,18 +232,16 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {264493039,30742221,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/Joker-development/Joker_development
+https://github.com/saadmzed/safaah-lebteam
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
+@saad7m [Founder]
+@mor33b599 [Developer]
+@lebteam  [chanall]
 
 Special thanks to
 awkward_potato
