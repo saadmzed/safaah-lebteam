a314f79ca6dc1633a14b9a09d3ee873ac74a9773
saadmzed <saad5mzed@gmail.com>
2017-04-13 00:12:42 +0300

    Update rebot.lua

--- plugins/rebot.lua
+++ plugins/rebot.lua
@@ -1,17 +1,17 @@
 --[[ 
 ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
 ▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀     BY(@AHMED_ALOBIDE)               ▀▄ ▄▀ 
-▀▄ ▄▀      BY(@hussian_9)                  ▀▄ ▄▀ 
+▀▄ ▄▀     BY(@mor33b599)                   ▀▄ ▄▀ 
+▀▄ ▄▀      BY(@saad7m)                     ▀▄ ▄▀ 
 ▀▄ ▄▀                                      ▀▄ ▄▀   
-▀▄ ▄▀          ملف الترحيب ع خاص البوت         ▀▄ ▄▀ 
+▀▄ ▄▀     ملف الترحيب ع خاص البوت         ▀▄ ▄▀ 
 ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
 --]]
 do 
 local function run(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "- Welcome to LAND💫 OF💫 HELL !\n\n- للمعلومات او الاستفسار على اليوزرات \n\n- 💠DEV : @AHMED_ALOBIDE  \n\n- 💠DEV : @hussian_9 "
+     return "- Welcome to LAND💫 OF💫 HELL !\n\n- للمعلومات او الاستفسار على اليوزرات \n\n- 💠DEV : @mor33b599  \n\n- 💠DEV : @saad7m "
 
     end 
   end 
